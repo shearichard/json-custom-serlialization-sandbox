882a3a4c71dbe51a30e0b66f4e0bf42f3e1a50d3
Richard Shea <rshea@thecubagroup.com>
2016-07-14 08:57:27 +1200

    Initial commit

---
+++ json-customobject-sandbox.js
@@ -0,0 +1,39 @@
+$( document ).ready(function() {
+    IndctrOrdElem = function(indId, panelForm){
+        //0..N of these are stored in `_indicatorOrdering`
+        //to record which order the panels should be rendered
+        //in.
+        this.indicatorid = indId;
+        this.panelform = panelForm;
+        this.getAttr = function(){
+            return this.indicatorid;
+        },
+        toJSON = function(){
+            return {attr: this.getAttr()}; // everything that needs to get stored
+        }
+    };
+    /***********************************************
+    IndctrOrdElem.prototype.toJSON() {
+        return {attr: this.getAttr()}; // everything that needs to get stored
+    };
+    IndctrOrdElem.fromJSON = function(obj) {
+        if (typeof obj == "string") obj = JSON.parse(obj);
+        var instance = new IndctrOrdElem;
+        instance._attr = obj.attr;
+        return instance;
+    };    
+    ************************************************/
+    //
+    var arr = [];
+    arr.push(new IndctrOrdElem(1, 101));
+    arr.push(new IndctrOrdElem(2, 102));
+    arr.push(new IndctrOrdElem(3, 103));
+    arr.push(new IndctrOrdElem(4, 104));
+    arr.push(new IndctrOrdElem(5, 105));
+    //
+    var str = "{'data': {'myDashIndctrOrdrngArray':" + arr + "}}";
+    //
+    var jayson = JSON.stringify(str);
+    //
+    console.log(jayson);
+});
